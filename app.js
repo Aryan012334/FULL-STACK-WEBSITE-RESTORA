ebcb99334ff6554c0a3f1db2053a3edb70fcbc5e
Aryan Yewale <aryanyewale07@gmail.com>
2025-01-29 18:21:01 +0530

    phase1- p1 done

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ async function main() {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
